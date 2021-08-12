4eaf69a29d1c09b80f64214efd6b685ed0cbb18d
shaonli8 <72174470+shaonli8@users.noreply.github.com>
2021-08-12 16:39:44 +0530

    Create Custom Button Stylesheet.js

---
+++ Custom Button Stylesheet.js
@@ -0,0 +1,23 @@
+const styles = StyleSheet.create({
+  button: {
+    width: "75%",
+    height: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 25,
+    backgroundColor: "#fff",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 8
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 10.32,
+    elevation: 16
+  },
+  buttonText: {
+    fontSize: 25,
+    color: "#6fc0b8",
+    fontWeight: "500"
+  }
+});
