96a1fea2db969b001402375f43b1825caa54e368
shaonli8 <72174470+shaonli8@users.noreply.github.com>
2021-08-12 16:44:54 +0530

    Create Settings-stylesheet.js

---
+++ Settings-stylesheet.js
@@ -0,0 +1,24 @@
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  upperContainer: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center"
+  },
+  input: {
+    width: "75%",
+    height: 55,
+    borderColor: "#6fc0b8",
+    borderWidth: 1.5,
+    marginTop: 20
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: "#6fc0b8"
+  },
+  buttonTitle: {
+    color: "#fff"
+  }
+});
